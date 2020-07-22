c4cdc3e4730419d4814fee7c7291c5167a16efb3
Aakil Fernandes <aakilfernandes@gmail.com>
2020-07-22 14:39:43 -0400

    init commit

---
+++ src/components/App/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './index.scss'
+
+interface AppComponentProps {}
+
+export class AppComponent extends React.Component {
+  render() {
+    return (
+      <div>
+        Hello World
+      </div>
+    )
+  }
+}
